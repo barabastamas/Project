d4b7fc2e257108f661c71b75c715b7440c23cf0b
Gaborata <atakukac@yahoo.com>
2015-11-17 13:41:44 +0200

    MainFragment added

---
+++ app/src/main/java/com/engineering/software/sapi/project/MainFragment.java
@@ -0,0 +1,34 @@
+package com.engineering.software.sapi.project;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment {
+
+
+    public MainFragment() {
+        // Required empty public constructor
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v  = inflater.inflate(R.layout.fragment_main, container, false);
+        Button addNewButton = (Button)v.findViewById(R.id.addNewButton);
+        // Inflate the layout for this fragment
+        return v;
+    }
+
+
+}

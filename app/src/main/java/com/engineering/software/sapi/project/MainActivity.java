54081da3b7ea66b8e61f724956004a04321e10dd
birozsolt <biro_zslt@yahoo.com>
2015-11-19 10:22:20 +0200

    profile fragmend added to Main Activity

--- app/src/main/java/com/engineering/software/sapi/project/MainActivity.java
+++ app/src/main/java/com/engineering/software/sapi/project/MainActivity.java
@@ -7,12 +7,10 @@ import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
@@ -102,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
         Class fragmentClass = null;
         switch (item.getItemId()) {
             case R.id.profile:
-                fragmentClass = EditRouteFragment.class;
+                fragmentClass = ProfileFragment.class;
                 break;
             case R.id.own_routes:
                 fragmentClass = OwnRoutesFragment.class;

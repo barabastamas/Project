e5b544455ad87092959e3910c6ac7a260cca1036
Barabas Tamas Janos <tamas_27@yahoo.co.uk>
2015-12-20 13:08:00 +0200

    Update Detail Route - Update FAB movement:  - Move up on recycler view scroll. After 2 seconds moves back to original position.

--- app/src/main/java/com/engineering/software/sapi/project/DetailRouteFragment.java
+++ app/src/main/java/com/engineering/software/sapi/project/DetailRouteFragment.java
@@ -22,11 +22,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
